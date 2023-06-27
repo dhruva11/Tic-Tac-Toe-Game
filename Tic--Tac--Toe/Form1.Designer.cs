03a0aa4c249fbe597fc93cfb82b0c0ab2a300415
Dhruv <dhruv.manoj9@gmail.com>
2023-06-27 20:47:48 +0530

    Add files via upload

---
+++ Tic--Tac--Toe/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace TicTacToe
+{
+    partial class TicTacToeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button0 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.checkBoxComputer = new System.Windows.Forms.CheckBox();
+            this.labelPlayer1 = new System.Windows.Forms.Label();
+            this.labelPlayer2 = new System.Windows.Forms.Label();
+            this.labelPlayer1Score = new System.Windows.Forms.Label();
+            this.labelPlayer2Score = new System.Windows.Forms.Label();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button0
+            // 
+            this.button0.Location = new System.Drawing.Point(50, 50);
+            this.button0.Name = "button0";
+            this.button0.Size = new System.Drawing.Size(50, 50);
+            this.button0.TabIndex = 0;
+            this.button0.UseVisualStyleBackColor = true;
+            this.button0.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(110, 50);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(50, 50);
+            this.button1.TabIndex = 1;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(170, 50);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(50, 50);
+            this.button2.TabIndex = 2;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(50, 110);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(50, 50);
+            this.button3.TabIndex = 3;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(110, 110);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(50, 50);
+            this.button4.TabIndex = 4;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(170, 110);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(50, 50);
+            this.button5.TabIndex = 5;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(50, 170);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(50, 50);
+            this.button6.TabIndex = 6;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(110, 170);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(50, 50);
+            this.button7.TabIndex = 7;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(170, 170);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(50, 50);
+            this.button8.TabIndex = 8;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // checkBoxComputer
+            // 
+            this.checkBoxComputer.AutoSize = true;
+            this.checkBoxComputer.Location = new System.Drawing.Point(50, 240);
+            this.checkBoxComputer.Name = "checkBoxComputer";
+            this.checkBoxComputer.Size = new System.Drawing.Size(99, 21);
+            this.checkBoxComputer.TabIndex = 9;
+            this.checkBoxComputer.Text = "Against AI";
+            this.checkBoxComputer.UseVisualStyleBackColor = true;
+            this.checkBoxComputer.CheckedChanged += new System.EventHandler(this.checkBoxComputer_CheckedChanged);
+            // 
+            // labelPlayer1
+            // 
+            this.labelPlayer1.AutoSize = true;
+            this.labelPlayer1.Location = new System.Drawing.Point(50, 280);
+            this.labelPlayer1.Name = "labelPlayer1";
+            this.labelPlayer1.Size = new System.Drawing.Size(69, 17);
+            this.labelPlayer1.TabIndex = 10;
+            this.labelPlayer1.Text = "Player 1:";
+            // 
+            // labelPlayer2
+            // 
+            this.labelPlayer2.AutoSize = true;
+            this.labelPlayer2.Location = new System.Drawing.Point(50, 310);
+            this.labelPlayer2.Name = "labelPlayer2";
+            this.labelPlayer2.Size = new System.Drawing.Size(69, 17);
+            this.labelPlayer2.TabIndex = 11;
+            this.labelPlayer2.Text = "Player 2:";
+            // 
+            // labelPlayer1Score
+            // 
+            this.labelPlayer1Score.AutoSize = true;
+            this.labelPlayer1Score.Location = new System.Drawing.Point(125, 280);
+            this.labelPlayer1Score.Name = "labelPlayer1Score";
+            this.labelPlayer1Score.Size = new System.Drawing.Size(16, 17);
+            this.labelPlayer1Score.TabIndex = 12;
+            this.labelPlayer1Score.Text = "0";
+            // 
+            // labelPlayer2Score
+            // 
+            this.labelPlayer2Score.AutoSize = true;
+            this.labelPlayer2Score.Location = new System.Drawing.Point(125, 310);
+            this.labelPlayer2Score.Name = "labelPlayer2Score";
+            this.labelPlayer2Score.Size = new System.Drawing.Size(16, 17);
+            this.labelPlayer2Score.TabIndex = 13;
+            this.labelPlayer2Score.Text = "0";
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(50, 350);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(170, 30);
+            this.buttonReset.TabIndex = 14;
+            this.buttonReset.Text = "Reset Game";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // TicTacToeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(270, 400);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.labelPlayer2Score);
+            this.Controls.Add(this.labelPlayer1Score);
+            this.Controls.Add(this.labelPlayer2);
+            this.Controls.Add(this.labelPlayer1);
+            this.Controls.Add(this.checkBoxComputer);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button0);
+            this.Name = "TicTacToeForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tic Tac Toe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button0;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.CheckBox checkBoxComputer;
+        private System.Windows.Forms.Label labelPlayer1;
+        private System.Windows.Forms.Label labelPlayer2;
+        private System.Windows.Forms.Label labelPlayer1Score;
+        private System.Windows.Forms.Label labelPlayer2Score;
+        private System.Windows.Forms.Button buttonReset;
+    }
+}
---
+++ Tic--Tac--Toe/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class TicTacToeForm : Form
+    {
+        private Button[] buttons;
+        private bool currentPlayer; // true for "X", false for "O"
+        private int moveCount;
+        private bool gameOver;
+        private bool isAgainstComputer;
+        private int player1Score;
+        private int player2Score;
+
+        public TicTacToeForm()
+        {
+            InitializeComponent();
+            InitializeGame();
+        }
+
+        private void InitializeGame()
+        {
+            buttons = new Button[] { button0, button1, button2, button3, button4, button5, button6, button7, button8 };
+            currentPlayer = true;
+            moveCount = 0;
+            gameOver = false;
+            isAgainstComputer = false;
+            player1Score = 0;
+            player2Score = 0;
+            UpdateScoreboard();
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            if (gameOver)
+                return;
+
+            Button button = (Button)sender;
+
+            if (button.Text != "")
+                return;
+
+            button.Text = currentPlayer ? "X" : "O";
+            currentPlayer = !currentPlayer;
+            moveCount++;
+
+            if (CheckWinCondition())
+            {
+                string winner = currentPlayer ? "O" : "X";
+                MessageBox.Show("Player " + winner + " wins!", "Game Over", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                gameOver = true;
+
+                if (currentPlayer) // Player 2 wins
+                    player2Score++;
+                else // Player 1 wins
+                    player1Score++;
+
+                UpdateScoreboard();
+            }
+            else if (moveCount == 9)
+            {
+                MessageBox.Show("It's a draw!", "Game Over", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                gameOver = true;
+            }
+
+            if (isAgainstComputer && !gameOver && !currentPlayer)
+            {
+                MakeComputerMove();
+            }
+        }
+
+        private bool CheckWinCondition()
+        {
+            List<int[]> winConditions = new List<int[]>
+            {
+                new int[] { 0, 1, 2 },
+                new int[] { 3, 4, 5 },
+                new int[] { 6, 7, 8 },
+                new int[] { 0, 3, 6 },
+                new int[] { 1, 4, 7 },
+                new int[] { 2, 5, 8 },
+                new int[] { 0, 4, 8 },
+                new int[] { 2, 4, 6 }
+            };
+
+            foreach (int[] condition in winConditions)
+            {
+                if (buttons[condition[0]].Text != "" && buttons[condition[0]].Text == buttons[condition[1]].Text && buttons[condition[1]].Text == buttons[condition[2]].Text)
+                    return true;
+            }
+
+            return false;
+        }
+
+        private void MakeComputerMove()
+        {
+            // Simple random move for the computer player
+            Random random = new Random();
+            int index = random.Next(0, 9);
+
+            while (buttons[index].Text != "")
+            {
+                index = random.Next(0, 9);
+            }
+
+            buttons[index].Text = currentPlayer ? "X" : "O";
+            currentPlayer = !currentPlayer;
+            moveCount++;
+
+            if (CheckWinCondition())
+            {
+                string winner = currentPlayer ? "O" : "X";
+                MessageBox.Show("Player " + winner + " wins!", "Game Over", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                gameOver = true;
+
+                if (currentPlayer) // Player 2 wins
+                    player2Score++;
+                else // Player 1 wins
+                    player1Score++;
+
+                UpdateScoreboard();
+            }
+            else if (moveCount == 9)
+            {
+                MessageBox.Show("It's a draw!", "Game Over", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                gameOver = true;
+            }
+        }
+
+        private void UpdateScoreboard()
+        {
+            labelPlayer1Score.Text = player1Score.ToString();
+            labelPlayer2Score.Text = player2Score.ToString();
+        }
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            foreach (Button button in buttons)
+            {
+                button.Text = "";
+            }
+
+            currentPlayer = true;
+            moveCount = 0;
+            gameOver = false;
+
+            if (isAgainstComputer && !currentPlayer)
+            {
+                MakeComputerMove();
+            }
+        }
+
+        private void checkBoxComputer_CheckedChanged(object sender, EventArgs e)
+        {
+            isAgainstComputer = checkBoxComputer.Checked;
+
+            if (isAgainstComputer && !currentPlayer)
+            {
+                MakeComputerMove();
+            }
+        }
+    }
+}
